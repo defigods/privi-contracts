e0da59aa07da9e560696c127deb01412ef77d13f
defigods <37310971+defigods@users.noreply.github.com>
2020-11-12 15:52:25 +0100

    adding mid cycle days to stakeTracker - work in progress

--- contracts/token/PRIVIPodToken.sol
+++ contracts/token/PRIVIPodToken.sol
@@ -22,10 +22,16 @@ contract PRIVIPodToken is Context, ERC20Burnable {
     string constant TOKEN_SYMBOL = 'PPT';
 
     // models
+    struct midCycleInput {
+        uint256 day;
+        uint256 acumulatedAmount;
+    }
+    
     struct stakeTracker {
         uint256 lastCyclePaid;
         uint256 rewards;
-        uint256 stakedBalance;
+        uint256 fullCycleBalance;
+        midCycleInput[] midCycleInputs;
     }
 
     address public parentFactory;
@@ -34,7 +40,8 @@ contract PRIVIPodToken is Context, ERC20Burnable {
     uint256 public totalTokenStaked;
     uint256 public liquidationDate;
     uint256 public lastCycleNumber;
-    mapping(address => stakeTracker) public trackedStakes;
+    uint256 public lastCycleDate;
+    mapping(address => stakeTracker) private trackedStakes;
     address[] public listOfStakers;
     mapping(uint256 => uint256) public interestPerCycle; // cycle -> interest amount
     
@@ -55,6 +62,9 @@ contract PRIVIPodToken is Context, ERC20Burnable {
         investToken = token;
         liquidationDate = date;
         isPodActive = true;
+        lastCycleDate = now.sub(2 days);
+        
+        _mint(msg.sender, 1000);
     }
     
     modifier onlyFactory() {
@@ -65,13 +75,22 @@ contract PRIVIPodToken is Context, ERC20Burnable {
     modifier updateStakingInterest(address account) {
         // if it is first time i.e there is no staked; this modifier passes
         // if there is staked; this modifier calculate the remaining rewards if there is any
-        if (trackedStakes[account].stakedBalance > 0) {
+        if (trackedStakes[account].fullCycleBalance > 0) {
             (uint256 rewards, , ) = getUnPaidStakingInterest();
             trackedStakes[account].rewards = trackedStakes[account].rewards.add(rewards);
             trackedStakes[account].lastCyclePaid = lastCycleNumber;
         }
+
         // we can have a condition for in case invetor want to invest mid-cycle
         _;
+    }
+    
+    function getAccountStakeTracker(address account) public view returns(uint256 lastCyclePaid, uint256 reward, uint256 fullCycleBalance, midCycleInput[] memory midCycleInputs) {
+        stakeTracker memory accountTracker = trackedStakes[account];
+        lastCyclePaid = accountTracker.lastCyclePaid;
+        reward = accountTracker.rewards;
+        fullCycleBalance = accountTracker.fullCycleBalance;
+        midCycleInputs = accountTracker.midCycleInputs;
     }
 
     function getUnPaidStakingInterest() public view returns(uint256 rewards, uint256 startCycle, uint256 endCycles) {
@@ -80,7 +99,7 @@ contract PRIVIPodToken is Context, ERC20Burnable {
         startCycle = trackedStakes[_msgSender()].lastCyclePaid.add(1);
         endCycles = startCycle.add(lastCycleNumber.sub(trackedStakes[_msgSender()].lastCyclePaid));
             for (uint256 i = startCycle; i <= endCycles; i++) {
-                totalReward = totalReward.add( interestPerCycle[i].mul(trackedStakes[_msgSender()].stakedBalance) );
+                totalReward = totalReward.add( interestPerCycle[i].mul(trackedStakes[_msgSender()].fullCycleBalance) );
             }
         
         rewards = totalReward;
@@ -89,6 +108,7 @@ contract PRIVIPodToken is Context, ERC20Burnable {
     function setCycleInterest(uint256 cycle, uint256 rewardAmountPerCycle) public  onlyFactory {
         require(cycle == lastCycleNumber.add(1), "PRIVIPodToken: Cycle number is not the next cycle number.");
         lastCycleNumber = cycle;
+        lastCycleDate = now;
         interestPerCycle[cycle] = rewardAmountPerCycle;
     }
 
@@ -105,21 +125,38 @@ contract PRIVIPodToken is Context, ERC20Burnable {
         _mint(account, amount);
     }
 
-    function stake(uint256 amount) public updateStakingInterest(_msgSender()) {
+    function stake(uint256 amount) public /*updateStakingInterest(_msgSender())*/ { // **<-|
         totalTokenStaked = totalTokenStaked.add(amount);
-        trackedStakes[_msgSender()].lastCyclePaid = lastCycleNumber;
-        trackedStakes[_msgSender()].stakedBalance = trackedStakes[_msgSender()].stakedBalance.add(amount);
+        trackedStakes[_msgSender()].lastCyclePaid = lastCycleNumber; // ** --A
+        //trackedStakes[_msgSender()].fullCycleBalance = trackedStakes[_msgSender()].fullCycleBalance.add(amount);
         _transfer(_msgSender(), address(this), amount);
+        
+        midCycleInput memory midCycle;
+        midCycle.day = (now.sub(lastCycleDate)).div(1 days);
+        midCycleInput[] memory lastInputs = trackedStakes[_msgSender()].midCycleInputs;
+        if (lastInputs.length > 0) { // there is no input yet
+           if (midCycle.day == lastInputs[(lastInputs.length).sub(1)].day ){
+                midCycle.acumulatedAmount = lastInputs[(lastInputs.length).sub(1)].acumulatedAmount.add(amount);
+                trackedStakes[_msgSender()].midCycleInputs[(trackedStakes[_msgSender()].midCycleInputs.length).sub(1)] = midCycle;
+            } else {
+                midCycle.acumulatedAmount = amount;
+                trackedStakes[_msgSender()].midCycleInputs.push(midCycle);
+            }
+        } else {
+            midCycle.acumulatedAmount = amount;
+            trackedStakes[_msgSender()].midCycleInputs.push(midCycle);
+        }
+        
         emit Staked(_msgSender(), amount);
     }
 
     function getStakedBalance() public view returns(uint256 staked) { 
-        staked = trackedStakes[_msgSender()].stakedBalance;
+        staked = trackedStakes[_msgSender()].fullCycleBalance;
     }
 
     function unStake(uint256 amount) public updateStakingInterest(_msgSender()) {
         totalTokenStaked = totalTokenStaked.sub(amount);
-        trackedStakes[_msgSender()].stakedBalance = trackedStakes[_msgSender()].stakedBalance.sub(amount);
+        trackedStakes[_msgSender()].fullCycleBalance = trackedStakes[_msgSender()].fullCycleBalance.sub(amount);
         _transfer(address(this), _msgSender(), amount);
         emit UnStaked(_msgSender(), amount);
     }
