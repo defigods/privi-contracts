38592e1ddbb1d58551d2d72e1d6260ab52dac4f4
defigods <37310971+defigods@users.noreply.github.com>
2021-02-12 15:45:46 +0100

    creating deploy scripts

---
+++ scripts/script_rinkeby.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Rnning migrations..."
+truffle migrate -f 2 --to 2 --network rinkeby
+truffle migrate -f 3 --to 3 --network rinkeby
+truffle migrate -f 4 --to 4 --network rinkeby
+truffle migrate -f 5 --to 5 --network rinkeby
+truffle migrate -f 6 --to 6 --network rinkeby
+truffle migrate -f 7 --to 7 --network rinkeby
+truffle migrate -f 8 --to 8 --network rinkeby
+truffle migrate -f 9 --to 9 --network rinkeby
+truffle migrate -f 10 --to 10 --network rinkeby
+truffle migrate -f 11 --to 11 --network rinkeby
---
+++ scripts/script_ropsten.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Rnning migrations..."
+truffle migrate -f 2 --to 2 --network ropsten
+truffle migrate -f 3 --to 3 --network ropsten
+truffle migrate -f 4 --to 4 --network ropsten
+truffle migrate -f 5 --to 5 --network ropsten
+truffle migrate -f 6 --to 6 --network ropsten
+truffle migrate -f 7 --to 7 --network ropsten
+truffle migrate -f 8 --to 8 --network ropsten
+truffle migrate -f 9 --to 9 --network ropsten
+truffle migrate -f 10 --to 10 --network ropsten
+truffle migrate -f 11 --to 11 --network ropsten
