495e476f0dea74b96973668d2959254547eb0d19
defigods <37310971+defigods@users.noreply.github.com>
2021-03-03 15:27:42 +0100

    implement bnb testnet config

--- truffle-config.js
+++ truffle-config.js
@@ -36,6 +36,20 @@ module.exports = {
 			timeoutBlocks: 500, // # of blocks before a deployment times out  (minimum/default: 50)
 			skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
 		},
+		bsc_testnet: {
+			provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+			network_id: 97,
+			confirmations: 1,
+			timeoutBlocks: 200,
+			skipDryRun: true
+		},
+		bsc: {
+			provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+			network_id: 56,
+			confirmations: 1,
+			timeoutBlocks: 200,
+			skipDryRun: true
+		},
 		advanced: {
 			websockets: true, // Enable EventEmitter interface for web3 (default: false)
 		},
