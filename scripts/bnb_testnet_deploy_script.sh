dc9dccf1bc3cedf8b2e2ef7058c196846ea5ce13
defigods <37310971+defigods@users.noreply.github.com>
2021-03-03 17:07:19 +0100

    deploy to rinkeby ropsten bnb_testnet

---
+++ scripts/bnb_testnet_deploy_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Rnning migrations..."
+truffle migrate -f 2 --to 2 --network bsc_testnet
+truffle migrate -f 3 --to 3 --network bsc_testnet
+truffle migrate -f 4 --to 4 --network bsc_testnet
+truffle migrate -f 5 --to 5 --network bsc_testnet
+truffle migrate -f 6 --to 6 --network bsc_testnet
+truffle migrate -f 7 --to 7 --network bsc_testnet
+truffle migrate -f 8 --to 8 --network bsc_testnet
+truffle migrate -f 9 --to 9 --network bsc_testnet
+truffle migrate -f 10 --to 10 --network bsc_testnet
+truffle migrate -f 11 --to 11 --network bsc_testnet
