8e9a8f7eb647f57c48cbed88ca6c4c0f0937a6df
defigods <defigodss@gmail.com>
2021-03-15 11:51:48 -0400

    merging from master, running formatters

--- contracts/token/PRIVIPodERC721TokenRoyalty.sol
+++ contracts/token/PRIVIPodERC721TokenRoyalty.sol
@@ -16,78 +16,96 @@ import "../abstracts/NFTRoyalty.sol";
  *
  */
 contract PRIVIPodERC721TokenRoyalty is Context, ERC721Burnable, NFTRoyalty {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdTracker;
-    address public parentFactory;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIdTracker;
+  address public parentFactory;
 
-    /**
-     * @dev Sets factory address
-     *
-     * See {ERC20-constructor}.
-     */
-    constructor(string memory name, string memory symbol, string memory baseURI, address factory, uint256 royaltyAmount, address creator) 
-        ERC721(name, symbol) 
-        NFTRoyalty(royaltyAmount, creator) {
+  /**
+   * @dev Sets factory address
+   *
+   * See {ERC20-constructor}.
+   */
+  constructor(
+    string memory name,
+    string memory symbol,
+    string memory baseURI,
+    address factory,
+    uint256 royaltyAmount,
+    address creator
+  ) ERC721(name, symbol) NFTRoyalty(royaltyAmount, creator) {
+    parentFactory = factory;
+    _setBaseURI(baseURI);
+  }
 
-        parentFactory = factory;
-        _setBaseURI(baseURI);
-    }
+  modifier onlyFactory() {
+    require(
+      _msgSender() == parentFactory,
+      "PRIVIPodERC721Token: Only Factory can call this function."
+    );
+    _;
+  }
 
-    modifier onlyFactory() {
-        require(_msgSender() == parentFactory, "PRIVIPodERC721Token: Only Factory can call this function.");
-        _;
-    }
+  /**
+   * @dev Creates `amount` new tokens for `to`.
+   *
+   * See {ERC20-_mint}.
+   *
+   * Requirements:
+   *
+   * - the caller must be factory only.
+   */
+  function mint(address to) public virtual onlyFactory {
+    // We cannot just use balanceOf to create the new tokenId because tokens
+    // can be burned (destroyed), so we need a separate counter.
+    _mint(to, _tokenIdTracker.current());
+    _tokenIdTracker.increment();
+  }
 
-    /**
-     * @dev Creates `amount` new tokens for `to`.
-     *
-     * See {ERC20-_mint}.
-     *
-     * Requirements:
-     *
-     * - the caller must be factory only.
-     */
-    function mint(address to) public virtual onlyFactory{
-        // We cannot just use balanceOf to create the new tokenId because tokens
-        // can be burned (destroyed), so we need a separate counter.
-        _mint(to, _tokenIdTracker.current());
-        _tokenIdTracker.increment();
-    }
+  /**
+   * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+   * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+   * Then pay royalty amount.
+   *
+   * Requirements:
+   *
+   * - approval from current owner of token id
+   *
+   * Emits a {RecievedRoyalties} event.
+   *
+   * Problems: TODO: may have reentrancy
+   */
+  function marketSell(
+    uint256 sellAmount,
+    uint256 tokenId,
+    address from,
+    address to
+  ) public payable {
+    // make sure all the fund is deposited here first
+    require(
+      msg.value == sellAmount,
+      "PRIVIPodERC721TokenRoyalty: royalty amount is not correct"
+    );
 
-    /**
-     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
-     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
-     * Then pay royalty amount.
-     *
-     * Requirements:
-     *
-     * - approval from current owner of token id
-     *
-     * Emits a {RecievedRoyalties} event.
-     * 
-     * Problems: TODO: may have reentrancy 
-     */
-    function marketSell(uint256 sellAmount, uint256 tokenId, address from, address to) public payable {
-        // make sure all the fund is deposited here first
-        require(msg.value == sellAmount, "PRIVIPodERC721TokenRoyalty: royalty amount is not correct");
-        
-        // pay creator
-        uint256 creatorShare = (msg.value * royalty_amount) / 100;  // TODO: not safe, must use safeMath
-        address payable royaltyOwner = payable(creator);
-        royaltyOwner.transfer(creatorShare);
-        
-        // pay current owner
-        payable(from).transfer(msg.value - creatorShare);
-        
-        // transfer token
-        safeTransferFrom(from, to, tokenId, bytes("royalty paid"));
-        
-        // event
-        royaltiesRecieved(creator, to, creatorShare);
-    }
-    
+    // pay creator
+    uint256 creatorShare = (msg.value * royalty_amount) / 100; // TODO: not safe, must use safeMath
+    address payable royaltyOwner = payable(creator);
+    royaltyOwner.transfer(creatorShare);
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual override(ERC721) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+    // pay current owner
+    payable(from).transfer(msg.value - creatorShare);
+
+    // transfer token
+    safeTransferFrom(from, to, tokenId, bytes("royalty paid"));
+
+    // event
+    royaltiesRecieved(creator, to, creatorShare);
+  }
+
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal virtual override(ERC721) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
 }
