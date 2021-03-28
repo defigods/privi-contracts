cd3b90a9143b486c41947000ee7c2b0000a300b5
defigods <defigodss@gmail.com>
2021-03-28 19:47:25 +0200

    Added tests for depositERC20Token() & withdrawERC20Token() in SwapManager

--- test/SwapManager.js
+++ test/SwapManager.js
@@ -10,6 +10,7 @@ const PodERC721Factory = artifacts.require('PRIVIPodERC721Factory');
 const PodERC721RoyaltyFactory = artifacts.require('PRIVIPodERC721RoyaltyFactory');
 const PodERC1155Factory = artifacts.require('PRIVIPodERC1155Factory');
 const PodERC1155RoyaltyFactory = artifacts.require('PRIVIPodERC1155RoyaltyFactory');
+const FakeUNI = artifacts.require('FakeUNI');
 
 contract('Swap Manager', (accounts) => {
     let swapManagerContract;
@@ -19,6 +20,7 @@ contract('Swap Manager', (accounts) => {
     let podERC721RoyaltyFactory;
     let podERC1155Factory;
     let podERC1155RoyaltyFactory;
+    let fakeUni;
     const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
     const [admin, mod1, investor1, hacker, ...rest] = accounts
 
@@ -34,12 +36,17 @@ contract('Swap Manager', (accounts) => {
     // };
 
     beforeEach(async () => {
+        // Bridge contract
         bridgeManagerContract = await BridgeManager.new({ from: admin });
+
+        // Factory contracts
         podERC20Factory = await PodERC20Factory.new(bridgeManagerContract.address, { from: admin });
         podERC721Factory = await PodERC721Factory.new(bridgeManagerContract.address, { from: admin });
         podERC721RoyaltyFactory = await PodERC721RoyaltyFactory.new(bridgeManagerContract.address, { from: admin });
         podERC1155Factory = await PodERC1155Factory.new(bridgeManagerContract.address, { from: admin });
         podERC1155RoyaltyFactory = await PodERC1155RoyaltyFactory.new(bridgeManagerContract.address, { from: admin });
+        
+        // Swap contract
         swapManagerContract = await SwapManager.new(
             bridgeManagerContract.address,
             podERC20Factory.address,
@@ -49,27 +56,71 @@ contract('Swap Manager', (accounts) => {
             podERC1155RoyaltyFactory.address,
             { from: admin }
         );
+
+        // Fake UNI contract
+        fakeUni = await FakeUNI.new(swapManagerContract.address, { from: admin });
+
+        // Give 1000 UNI tokens to investor1
+        await fakeUni.mintForUser(investor1, 1000, { from: admin });
+
+        // Register UNI token in Bridge
+        await bridgeManagerContract.registerTokenERC20(
+            'Uniswap',
+            'UNI',
+            fakeUni.address
+        );
     });
 
-    it('should not deposit ERC20 token if not registered in Bridge', async () => {
+    /*********** CHECK depositERC20Token() **************/
+
+    it('should not deposit ERC20 tokens if not registered in Bridge', async () => {
         await expectRevert(
-            swapManagerContract.depositERC20Token('SJS', 15, {from: investor1}),
+            swapManagerContract.depositERC20Token('NON_EXISTING', 15, { from: investor1 }),
             'SwapManager: token is not registered into the platform'
         );
     });
 
-    // it('should not deposit ERC20 token if not approved by user', async () => {
-    //     await bridgeManagerContract.registerTokenERC20(
-    //         'The SJS Token',
-    //         'SJS',
-    //         // Address
-    //     )
-    //     await expectRevert(
-    //         swapManagerContract.depositERC20Token('SJS', 15, {from: investor1}),
-    //         'SwapManager: token is not registered into the platform'
-    //     );
-    // });
+    it('should not deposit ERC20 tokens if not approved by user', async () => {
+        await expectRevert(
+            swapManagerContract.depositERC20Token('UNI', 15, { from: investor1 }),
+            'SwapManager: token amount to be transferred to PRIVI is not yet approved by User'
+        );
+    });
 
+    it('should deposit ERC20 tokens', async () => {
+        const balanceSwapBefore = await fakeUni.balanceOf(swapManagerContract.address); 
 
+        await fakeUni.approve(swapManagerContract.address, 300, { from: investor1 });
+        await swapManagerContract.depositERC20Token('UNI', 300, { from: investor1 });
+
+        const balanceSwapAfter = await fakeUni.balanceOf(swapManagerContract.address); 
+
+        assert(balanceSwapBefore.toString() === '0', 'Swap balance should be 0');
+        assert(balanceSwapAfter.toString() === '300', 'Swap balance should be 300');
+    });
+
+    /*********** CHECK withdrawERC20Token() **************/
+
+    it('should not withdraw ERC20 tokens - only TRANSFER_ROLE', async () => {
+        await expectRevert(
+            swapManagerContract.withdrawERC20Token('UNI', hacker, 15, { from: hacker }),
+            'SwapManager: must have TRANSFER_ROLE to withdraw token'
+        );
+    });
+
+    it('should withdraw ERC20 tokens', async () => {
+        const balanceSwapBefore = await fakeUni.balanceOf(swapManagerContract.address); 
+
+        await fakeUni.approve(swapManagerContract.address, 300, { from: investor1 });
+        await swapManagerContract.depositERC20Token('UNI', 300, { from: investor1 });
+        await swapManagerContract.withdrawERC20Token('UNI', investor1, 50, { from: admin });
+
+        const balanceSwapAfter = await fakeUni.balanceOf(swapManagerContract.address); 
+
+        assert(balanceSwapBefore.toString() === '0', 'Swap balance should be 0');
+        assert(balanceSwapAfter.toString() === '250', 'Swap balance should be 250');
+    });
+
+    // Remark: shouldn't SwapManager revert if not ERC20 token can be withdrawn?
 
 });
