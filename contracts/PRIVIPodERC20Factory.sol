5e00a7a85ba4236242bdd8622ed9005582b8080b
defigods <defigodss@gmail.com>
2021-04-01 22:15:02 +0200

    Removed safeMath library (replaced by pragma 0.8)

--- contracts/PRIVIPodERC20Factory.sol
+++ contracts/PRIVIPodERC20Factory.sol
@@ -3,12 +3,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./token/PRIVIPodERC20Token.sol";
 import "./interfaces/IBridgeManager.sol";
 
 contract PRIVIPodERC20Factory is AccessControl {
-  using SafeMath for uint256;
 
   bytes32 public constant MODERATOR_ROLE = keccak256("MODERATOR_ROLE");
   address public bridgeManagerAddress;
@@ -77,16 +75,16 @@ contract PRIVIPodERC20Factory is AccessControl {
     // TODO: is it Users or Admins to create them? what restrictions shall we applied to call this function?
     require(
       podTokenAddressesById[podId] == address(0),
-      "PRIVIPodERC20Factory: Pod id already exists."
+      "PRIVIPodERC20Factory: Pod id already exists"
     );
     require(
       podTokenAddressesBySymbol[podTokenSymbol] == address(0),
-      "PRIVIPodERC20Factory: Pod symbol already exists."
+      "PRIVIPodERC20Factory: Pod symbol already exists"
     );
     PRIVIPodERC20Token podToken =
       new PRIVIPodERC20Token(podTokenName, podTokenSymbol, address(this));
     podAddress = address(podToken);
-    totalPodCreated.add(1);
+    totalPodCreated += 1;
     podTokenAddressesById[podId] = podAddress;
     podTokenAddressesBySymbol[podTokenSymbol] = podAddress;
     IBridgeManager(bridgeManagerAddress).registerTokenERC20(
@@ -111,15 +109,15 @@ contract PRIVIPodERC20Factory is AccessControl {
   ) external {
     require(
       hasRole(MODERATOR_ROLE, _msgSender()),
-      "PRIVIPodERC20Factory: must have MODERATOR_ROLE to invest for investor."
+      "PRIVIPodERC20Factory: must have MODERATOR_ROLE to invest for investor"
     );
     require(
       account != address(0),
-      "PRIVIPodERC20Factory: Account address should not be zero."
+      "PRIVIPodERC20Factory: Account address should not be zero"
     );
     require(
       investAmount > 0,
-      "PRIVIPodERC20Factory: investAmount should not be zero."
+      "PRIVIPodERC20Factory: investAmount should not be zero"
     );
     PRIVIPodERC20Token(podTokenAddressesById[podId]).mint(
       account,
@@ -141,15 +139,15 @@ contract PRIVIPodERC20Factory is AccessControl {
   ) external {
     require(
       hasRole(MODERATOR_ROLE, _msgSender()),
-      "PRIVIPodERC20Factory: must have MODERATOR_ROLE to invest for investor."
+      "PRIVIPodERC20Factory: must have MODERATOR_ROLE to invest for investor"
     );
     require(
       account != address(0),
-      "PRIVIPodERC20Factory: Account address should not be zero."
+      "PRIVIPodERC20Factory: Account address should not be zero"
     );
     require(
       investAmount > 0,
-      "PRIVIPodERC20Factory: investAmount should not be zero."
+      "PRIVIPodERC20Factory: investAmount should not be zero"
     );
     PRIVIPodERC20Token(podTokenAddressesBySymbol[tokenSymbol]).mint(
       account,
